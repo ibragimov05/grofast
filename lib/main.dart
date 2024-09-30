460f2986cf0d2219bd1b27c8491e7b59679613e3
ibragimov05 <ibragimov.asus15@gmail.com>
2024-09-30 11:18:14 +0500

    firebase analytics has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 import 'app.dart';
 import 'core/utils/utils.dart';
 import 'dependency_setup.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+  } catch(e) {
+    print("Failed to initialize Firebase: $e");
+  }
+
   if (kDebugMode) Bloc.observer = LogBlocObserver();
 
   DependencySetup.init();
