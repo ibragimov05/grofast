978d3f69657077ed29d2b6a2e18f4b0274cfe6c9
ibragimov05 <ibragimov.asus15@gmail.com>
2024-10-01 09:47:22 +0500

    splash page has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:grofast/services/firebase_api.dart';
+import 'package:grofast/services/notification_service.dart';
 
 import 'app.dart';
 import 'core/utils/utils.dart';
@@ -10,13 +14,24 @@ import 'dependency_setup.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+    await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+
+    final firebaseAPI = FirebaseApi();
+
+    firebaseAPI.initNotifications();
+
+    // await FirebaseMessaging.instance.getInitialMessage();
+    // await FirebaseMessaging.instance.requestPermission();
+    // await NotificationService.initNotification();
+
+    // _getToken();
   } catch(e) {
     print("Failed to initialize Firebase: $e");
   }
@@ -25,5 +40,13 @@ void main() async {
 
   DependencySetup.init();
 
+  FlutterNativeSplash.remove();
+
   runApp(const App());
+}
+
+void _getToken() {
+  FirebaseMessaging.instance.getToken().then((value) {
+    print("TOKEN IS :: :: $value");
+  });
 }
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:grofast/features/home/presentation/pages/home_page.dart';
 import 'package:grofast/features/others/presentation/splash/splash_page.dart';
 
 part 'route_names.dart';
@@ -8,7 +9,7 @@ final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final GoRouter router = GoRouter(
   navigatorKey: rootNavigatorKey,
-  initialLocation: Routes.splash,
+  initialLocation: Routes.home,
   routes: [
     GoRoute(
       path: Routes.splash,
@@ -16,5 +17,11 @@ final GoRouter router = GoRouter(
       parentNavigatorKey: rootNavigatorKey,
       builder: (_, __) => const SplashPage(),
     ),
+    GoRoute(
+      path: Routes.home,
+      name: Routes.home,
+      parentNavigatorKey: rootNavigatorKey,
+      builder: (_, __) => const HomePage(),
+    )
   ],
 );
